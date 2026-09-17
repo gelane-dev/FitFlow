35bbbf66935f1a96a462391b75ff2df12aae31b7
gelane-dev <leonardo.gelane.santos@gmail.com>
2026-09-17 15:12:33 -0300

    docs: remove README padrão do Expo

--- app/routers/main.py
+++ app/routers/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, usuarios, admin, exercicios, fichas, treino
 
 app = FastAPI(
     title="Academia API",
     description="API gerenciamento de academia",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router)
