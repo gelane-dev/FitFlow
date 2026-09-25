6f8c36db74e9bb463742151d2ddf3a736ea4bddb
gelane-dev <leonardo.gelane.santos@gmail.com>
2026-09-25 14:33:59 -0300

    feat: adiciona operações de gerenciamento de exercícios

--- app-academia-mobile/src/services/exercicio.service.ts
+++ app-academia-mobile/src/services/exercicio.service.ts
@@ -2,6 +2,8 @@ import { api } from './api';
 
 import {
   Exercicio,
+  ExercicioAtualizar,
+  ExercicioCriar,
   ExerciciosListaResposta,
 } from '@/types/exercicio.types';
 
@@ -9,13 +11,13 @@ export async function buscarExercicios(
   paginaSolicitada: number
 ): Promise<ExerciciosListaResposta> {
   const resposta = await api.get<ExerciciosListaResposta>(
-    '/exercicios', 
-    { 
-    params: {
-      pagina: paginaSolicitada,
-    },
-  }
-);
+    '/exercicios',
+    {
+      params: {
+        pagina: paginaSolicitada,
+      },
+    }
+  );
 
   return resposta.data;
 }
@@ -28,4 +30,33 @@ export async function buscarExercicio(
   );
 
   return resposta.data;
+}
+
+export async function criarExercicio(
+  dados: ExercicioCriar
+): Promise<Exercicio> {
+  const resposta = await api.post<Exercicio>(
+    '/exercicios',
+    dados
+  );
+
+  return resposta.data;
+}
+
+export async function atualizarExercicio(
+  id: number,
+  dados: ExercicioAtualizar
+): Promise<Exercicio> {
+  const resposta = await api.put<Exercicio>(
+    `/exercicios/${id}`,
+    dados
+  );
+
+  return resposta.data;
+}
+
+export async function deletarExercicio(
+  id: number
+): Promise<void> {
+  await api.delete(`/exercicios/${id}`);
 }
